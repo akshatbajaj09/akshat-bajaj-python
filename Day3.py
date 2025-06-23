c964ff436292f6535cb1b5592163c3598d66707a
Akshat Bajaj <akshatbajaj108@gmail.com>
2025-06-23 22:31:40 +0530

    Formatted Strings.

--- Day3.py
+++ Day3.py
@@ -1,1 +1,12 @@
+# Formatted strings
+name = 'Akshat Bajaj'
+age = str(20)
+# Printing without using formatted strings (tedious).
+print('Hi ' + name + '. You are ' + age + ' years old.') 
+print('\n')
+# Printing using formatted strings by .format() method (easier).
+print('Hi {}. You are {} years old'.format(name, age))
+print('\n')
+# Printing using the f action (easiest).
+print(f'Hi {name}. You are {age} years old.') 
 
